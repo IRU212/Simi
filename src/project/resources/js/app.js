851faa23a51d13aaf60fd319c6c300230b949d39
IRU212 <ryuuyapro@gmail.com>
2023-04-24 20:25:37 +0900

    react docker 環境構築

--- src/project/resources/js/app.js
+++ src/project/resources/js/app.js
@@ -1,1 +1,10 @@
-import './bootstrap';
+require('./bootstrap');
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Example from './components/Example';
+
+
+if (document.getElementById('app')) {
+  ReactDOM.render(<Example />, document.getElementById('app'));
+}
