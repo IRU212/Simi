f84cbf7e727c050b09fdc370e22005b26174dee6
IRU212 <ryuuyapro@gmail.com>
2023-04-24 22:06:04 +0900

    react-router-dom@v6

--- src/project/resources/js/components/Example.jsx
+++ src/project/resources/js/components/Example.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Header from './templates/Header';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './views/Home';
+import Setting from './views/Setting';
 
 function Example() {
     return (
         <div>
-            aaa
+            <BrowserRouter>
+                <Routes>
+
+                    {/* ホーム */}
+                    <Route path="/" element={<Home />} />
+
+                    {/* 設定 */}
+                    <Route path="/setting" element={<Setting />} />
+
+                </Routes>
+            </BrowserRouter>
         </div>
     );
 }
