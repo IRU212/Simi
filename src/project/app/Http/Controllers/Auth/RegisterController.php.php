fc5cb5962ef26f134dfeb98cc387663afea864ca
IRU212 <ryuuyapro@gmail.com>
2023-04-26 14:48:21 +0900

    User model 変更

---
+++ src/project/app/Http/Controllers/Auth/RegisterController.php.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class RegisterController.php extends Controller
+{
+    /**
+     * アカウント新規登録
+     *
+     * @return void
+     */
+    public function store()
+    {
+        # code...
+    }
+}
--- src/project/app/Models/User.php
+++ src/project/app/Models/User.php
@@ -10,35 +10,32 @@ use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    /**
+     * テーブル名
+     *
+     * @var string
+     */
+    protected $table = "user";
 
     /**
-     * The attributes that are mass assignable.
+     * 利用するカラム名
      *
-     * @var array<int, string>
+     * @var array
      */
     protected $fillable = [
         'name',
         'email',
         'password',
+        'icon_image',
+        'back_image'
     ];
 
     /**
-     * The attributes that should be hidden for serialization.
+     * 非表示にするカラム
      *
-     * @var array<int, string>
+     * @var array
      */
     protected $hidden = [
         'password',
-        'remember_token',
-    ];
-
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
     ];
 }
