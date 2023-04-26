f9d43377dc360f2759529dbadedbbe37a66c66e1
IRU212 <ryuuyapro@gmail.com>
2023-04-26 22:26:22 +0900

    user テーブル編集

---
+++ src/project/database/migrations/2023_04_26_131917_modify_user_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class ModifyUserTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('user', function (Blueprint $table) {
+            $table->string('icon_image')->nullable()->change();;
+            $table->string('back_image')->nullable()->change();;
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('user', function (Blueprint $table) {
+            //
+        });
+    }
+}
