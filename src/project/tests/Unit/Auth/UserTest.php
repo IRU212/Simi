ccd19a8fc4e077195a9b6dac869ec1af07fe6191
IRU212 <ryuuyapro@gmail.com>
2023-05-05 12:36:54 +0900

    userController テスト　作成

--- src/project/tests/Unit/Auth/UserTest.php
+++ src/project/tests/Unit/Auth/UserTest.php
@@ -6,16 +6,44 @@ use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithoutMiddleware;
 
+use App\Models\User;
+
 class UserTest extends TestCase
 {
     /**
-     * A basic unit test example.
+     * ユーザログイン情報
      *
      * @return void
      */
-    public function test_example()
+    public function test_user_index()
     {
-        $response = $this->get('/api/user/index');
+        $response = $this->get(route('user.index'));
+
+        $response->assertStatus(200);
+    }
+
+    /**
+     * ユーザプロフィール情報
+     *
+     * @return void
+     */
+    public function test_user_show()
+    {
+        $response = $this->get(route('user.show',4));
+
+        $response->assertStatus(200);
+    }
+
+    /**
+     * ユーザプロフィール編集
+     *
+     * @return void
+     */
+    public function test_user_edit()
+    {
+        $response = $this->post(route('user.edit'),[
+            'name' => 'ccccc'
+        ]);
 
         $response->assertStatus(200);
     }
