cb66222a1758ea532537d8f5b8006f8584fcd575
IRU212 <ryuuyapro@gmail.com>
2023-05-05 15:26:10 +0900

    question_reply テーブル作成

---
+++ src/project/database/migrations/2023_05_05_062023_create_question_reply_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateQuestionReplyTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('question_reply', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('body');
+            $table->text('image');
+            $table->foreignId('user_id')
+            $table->foreignId('question_id')
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('question_reply');
+    }
+}
