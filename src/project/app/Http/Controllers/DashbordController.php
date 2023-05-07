dcfa12f651cab5d669ba56c814bc9ab490a93f36
IRU212 <ryuuyapro@gmail.com>
2023-05-07 11:10:46 +0900

    ダッシュボード　質問数取得　バック

--- src/project/app/Http/Controllers/DashbordController.php
+++ src/project/app/Http/Controllers/DashbordController.php
@@ -4,7 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-use App\Models\Like\Question;
+use App\Models\Question;
+use App\Models\Like\Question as LikeQuestion;
 use App\Models\Follow;
 
 class DashbordController extends Controller
@@ -12,12 +13,14 @@ class DashbordController extends Controller
     public function index()
     {
         // モデル　インスタンス呼び出し
-        $follow = new Follow();
         $question = new Question();
+        $follow = new Follow();
+        $question_like = new LikeQuestion();
 
         $data = [
-            'like' => $question->where('user_id','=',session('login_id')[0])->count(),
-            'follower' => $follow->where('user_id','=',session('login_id')[0])->count()
+            'question' => $question->where('user_id','=',session('login_id')[0])->count(), // 質問投稿数
+            'like' => $question_like->where('user_id','=',session('login_id')[0])->count(), // いいね数
+            'follower' => $follow->where('user_id','=',session('login_id')[0])->count() // フォロー数
         ];
 
         // JSONで返す
