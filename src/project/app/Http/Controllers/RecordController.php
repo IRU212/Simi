46cf88a1e83bd237fa2ca70c59a0bbd7c9f9bf38
IRU212 <ryuuyapro@gmail.com>
2023-05-25 23:37:45 +0900

    月ごとの勉強時間取得

--- src/project/app/Http/Controllers/RecordController.php
+++ src/project/app/Http/Controllers/RecordController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use App\Models\Record;
 use App\Models\Book;
 use Illuminate\Http\Request;
+use Carbon\Carbon;
+use DB;
 
 class RecordController extends Controller
 {
@@ -13,9 +15,42 @@ class RecordController extends Controller
         // モデルインスタンス呼び出し
         $record = new Record();
 
-        $data = $record->get();
+        // 月ごとの勉強時間を取得
+        $month_each_data = $record->select(
+                            DB::raw("SEC_TO_TIME(SUM(TIME_TO_SEC(time))) as time"),
+                            DB::raw("MONTHNAME(created_at) as month")
+                        )
+                        ->whereYear('created_at', date('Y'))
+                        ->groupBy('month')
+                        ->get();
 
-        return response()->json($data, 200);
+        // 1月から12月までの月配列
+        $month_array = [
+            ["month" => "January", "time" => "00:00:00"],
+            ["month" => "February", "time" => "00:00:00"],
+            ["month" => "March", "time" => "00:00:00"],
+            ["month" => "April", "time" => "00:00:00"],
+            ["month" => "May", "time" => "00:00:00"],
+            ["month" => "June", "time" => "00:00:00"],
+            ["month" => "July", "time" => "00:00:00"],
+            ["month" => "August", "time" => "00:00:00"],
+            ["month" => "September", "time" => "00:00:00"],
+            ["month" => "October", "time" => "00:00:00"],
+            ["month" => "November", "time" => "00:00:00"],
+            ["month" => "December", "time" => "00:00:00"]
+        ];
+
+        // DB取得データ
+        foreach ($month_each_data as $index => $item) {
+
+            // DB取得データ　月の位置を取得
+            $month_array_position = array_search($item["month"], array_column($month_array,"month"));
+
+            $month_array[$month_array_position] = $item;
+
+        }
+
+        return response()->json($month_array, 200);
     }
 
     /**
