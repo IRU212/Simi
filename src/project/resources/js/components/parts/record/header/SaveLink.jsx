7f6f0c5a146d9ac852e44d03e1e4a7ea0dcfddf1
IRU212 <ryuuyapro@gmail.com>
2023-05-29 23:19:23 +0900

    勉強記録　リンク material icon 追加

--- src/project/resources/js/components/parts/record/header/SaveLink.jsx
+++ src/project/resources/js/components/parts/record/header/SaveLink.jsx
@@ -1,11 +1,17 @@
 import styles from '../../../../../../public/scss/parts/record.module.scss'
 import { Link } from 'react-router-dom'
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 
 // 記録保存リンク
 export default function SaveLink() {
     return (
         <Link to="/record/save" className={styles.link}>
-            勉強を記録する
+            <div>
+                勉強を記録する
+            </div>
+            <div>
+                <ArrowForwardIosIcon />
+            </div>
         </Link>
     )
 }
